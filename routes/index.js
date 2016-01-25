2e95d3c9e71eeeed46ebcefc26a48a7c269bccf6
Darren Lewis <darren@octono.com>
2016-01-25 19:39:35 +0000

    Added view for last.jpg

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,21 +8,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/last',function(req, res, next){
-    var options = {
-    root: './public/images/',
-    dotfiles: 'deny',
-    headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true,
-        'cache-control':'no-cache'
-    }
-  };
 
-  var fileName = 'last.jpg';
-  res.sendFile(fileName, options, function (err) {
-    if (err) {
-      res.status(err.status).end();
-    }
+  fs.stat('public/images/last.jpg',function (err, stats) {
+    res.render('last', {modified:stats.mtime});
   });
+  //   var options = {
+  //   root: './public/images/',
+  //   dotfiles: 'deny',
+  //   headers: {
+  //       'x-timestamp': Date.now(),
+  //       'x-sent': true,
+  //       'cache-control':'no-cache'
+  //   }
+  // };
+  //
+  // var fileName = 'last.jpg';
+  // res.sendFile(fileName, options, function (err) {
+  //   if (err) {
+  //     res.status(err.status).end();
+  //   }
+  // });
 });
 module.exports = router;
